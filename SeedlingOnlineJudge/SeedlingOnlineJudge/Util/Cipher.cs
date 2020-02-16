326ee46de358703707e4f419ab188cd6f1deb5d9
Weiss <felipe.weiss@vtex.com.br>
2020-02-16 09:51:33 -0300

    add new crypt

--- SeedlingOnlineJudge/SeedlingOnlineJudge/Util/Cipher.cs
+++ SeedlingOnlineJudge/SeedlingOnlineJudge/Util/Cipher.cs
@@ -1,31 +1,62 @@
 ﻿using Microsoft.AspNetCore.DataProtection;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SeedlingOnlineJudge.Util
 {
     public class Cipher
     {
-        private readonly IDataProtectionProvider _dataProtectionProvider;
-        private const string Key = "9rrO8m1lPwRVVb4dyoCAl1ZszlwjgvRl";
-
-        public Cipher(IDataProtectionProvider dataProtectionProvider)
+        public Cipher()
         {
-            _dataProtectionProvider = dataProtectionProvider;
         }
 
-        public string Encrypt(string input)
+        public string Encrypt(string clearText)
         {
-            var protector = _dataProtectionProvider.CreateProtector(Key);
-            return protector.Protect(input);
+            string EncryptionKey = "abc123";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
         }
-
         public string Decrypt(string cipherText)
         {
-            var protector = _dataProtectionProvider.CreateProtector(Key);
-            return protector.Unprotect(cipherText);
+            string EncryptionKey = "abc123";
+            cipherText = cipherText.Replace(" ", "+");
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
         }
     }
 }
